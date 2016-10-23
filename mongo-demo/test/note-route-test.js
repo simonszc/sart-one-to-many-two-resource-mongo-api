8397fbda9049102f8b04499fddb36ff05c3c6c8c
simonszc <zsimonson26@gmail.com>
2016-10-22 23:35:28 -0700

    added put note route. delete to come

--- mongo-demo/test/note-route-test.js
+++ mongo-demo/test/note-route-test.js
@@ -19,6 +19,9 @@ const exampleList = {
   name: 'lulwat',
   timestamp: new Date()
 };
+const updateNote = {
+  name: 'constantine'
+}
 
 
 describe('testing note routes', function() {
@@ -83,6 +86,46 @@ describe('testing note routes', function() {
         .end((err, res) => {
           if(err) return done(err);
           expect(res.body.name).to.equal(exampleNote.name);
+          expect(res.body.listID).to.equal(this.tempList._id.toString());
+          expect(res.body._id).to.equal(this.tempNote._id.toString());
+          done();
+        });
+      });
+    });
+  });
+  describe('testing PUT requests', function() {
+    describe('with valid list id and noteBody', function() {
+      before( done => {
+        new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+        })
+        .then( () => {
+          exampleNote.listID = this.tempList._id;
+          new Note(exampleNote).save()
+          .then( note => {
+            this.tempNote = note;
+            done();
+          })
+          .catch(done);
+        })
+        .catch(done);
+      });
+      after( done => {
+        Promise.all([
+          List.remove({}),
+          Note.remove({})
+        ])
+        .then(() => done())
+        .catch(done);
+      });
+      it('should return a note', (done) => {
+        request.put(`${url}/api/list/${this.tempList.id}/note/${this.tempNote.id}`)
+        .send(updateNote)
+        .end((err, res) => {
+          if(err) return done(err);
+          expect(res.body.name).to.equal(updateNote.name);
+          expect(res.body.content).to.equal(exampleNote.content);
           expect(res.body.listID).to.equal(this.tempList._id.toString());
           expect(res.body._id).to.equal(this.tempNote._id.toString());
           done();
